ae0284ba064fbc1f3020c3256de75b09444a0ae0
Ryan Dalrymple <ryan.d235711@gmail.com>
2024-05-20 12:39:46 -0600

    Implemented Logistic Regression and fixed bug with very low accuracy on Random Forest. Accuracy on both algorithms is greater than 98%. Looking for alternative evaluations because this number is very high.

--- CudaLearning.py
+++ CudaLearning.py
@@ -13,6 +13,7 @@ import DataPreprocessor.Preprocessor as pp
 
 from cuml.metrics import accuracy_score
 from cuml.ensemble import RandomForestClassifier
+from cuml.linear_model import LogisticRegression
 
 #Set random seeds
 cp.random.seed(31)
@@ -38,9 +39,12 @@ testFeatures, testLabels         = pp.extractLabels(testData, 'Crop')
 #label0 = label0[1]
 label0 = trainingLabels.values.flatten()
 
-#~~RANDOM FOREST~~ TODO Cross validation, Accuracy is low, look into it.
-print("Training Decision Tree Regressor model")
-model   = RandomForestClassifier(max_depth=5, n_estimators=50, random_state=31)
+#TODO Cross validation, Accuracy is low, look into it.
+#TODO: Implement linear regression, decision tree, random forest, gradient boosting machine, support vector regression, and neural network
+
+#~~RANDOM FOREST~~
+print("\n\nTraining Decision Tree Regressor model")
+model   = RandomForestClassifier(max_depth=5, n_estimators=50, random_state=31, n_streams=1, split_criterion=1) #TODO: for CV, check gini index (split_criterion=0) and entropy (1)
 start   = time.time()
 model.fit(trainingFeatures, trainingLabels.values.flatten())
 end     = time.time()
@@ -49,7 +53,7 @@ print(f"Training the Random Forest took {round(elapsed, 2)} seconds.")
 
 print("Making predictions on the testing set")
 start   = time.time()
-predictions = model.predict(testFeatures.values.flatten())
+predictions = model.predict(testFeatures)
 end     = time.time()
 elapsed = end-start
 print(f"Making predictions took {round(elapsed,2)} seconds.")
@@ -57,9 +61,23 @@ print(f"Making predictions took {round(elapsed,2)} seconds.")
 #Metrics
 accuracy = accuracy_score(testLabels, predictions)
 print(f"Accuracy for the Random Forest is: {round(accuracy, 4)}")
-'''print("Classification Report:")
-print(classification_report(testLabels, predictions))
-print("Confusion Matrix:")
-print(confusion_matrix(testLabels, predictions))'''
 
-#TODO: Implement linear regression, decision tree, random forest, gradient boosting machine, support vector regression, and neural network
+#~~LOGISTIC REGRESSION~~
+print("\n\nTraining Logistic Regression model")
+model   = LogisticRegression()
+start   = time.time()
+model.fit(trainingFeatures, trainingLabels.values.flatten())
+end     = time.time()
+elapsed = end-start
+print(f"Training Logistic Regression took {round(elapsed, 2)} seconds.")
+
+print("Making predictions on the testing set")
+start   = time.time()
+predictions = model.predict(testFeatures)
+end     = time.time()
+elapsed = end-start
+print(f"Making predictions took {round(elapsed,2)} seconds.")
+
+#Metrics
+accuracy = accuracy_score(testLabels, predictions)
+print(f"Accuracy for Logistic Regression is: {round(accuracy, 4)}")
